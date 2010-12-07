fc9a945718844a0c857c844da7736ac7d634b63c
Chris AtLee <catlee@mozilla.com>
2010-12-06 22:59:54 -0500

    Implement parallelization across masters using multiprocessing

--- buildbot-related/manage_masters.py
+++ buildbot-related/manage_masters.py
@@ -3,6 +3,11 @@ import master_fabric
 from fabric.api import env
 from fabric.context_managers import settings
 
+def run_action_on_master(action, master):
+    action_func = getattr(master_fabric, action)
+    with settings(host_string=master['hostname']):
+        return action_func(master)
+
 if __name__ == '__main__':
     from optparse import OptionParser
     import textwrap
@@ -19,12 +24,17 @@ Supported actions:
     parser.set_defaults(
             hosts=[],
             roles=[],
+            concurrency=1,
             )
     parser.add_option("-f", "--master-file", dest="master_file", help="list/url of masters")
     parser.add_option("-H", "--host", dest="hosts", action="append")
     parser.add_option("-R", "--role", dest="roles", action="append")
+    parser.add_option("-j", dest="concurrency", type="int")
 
     options, actions = parser.parse_args()
+
+    if options.concurrency > 1:
+        import multiprocessing
 
     if not options.master_file:
         parser.error("master-file is required")
@@ -51,7 +61,21 @@ Supported actions:
 
     env.user = 'cltbld'
     for action in actions:
-        action_func = getattr(master_fabric, action)
-        for master in masters:
-            with settings(host_string=master['hostname']):
-                action_func(master)
+        if options.concurrency == 1:
+            for master in masters:
+                run_action_on_master(action, master)
+        else:
+            results = {}
+            p = multiprocessing.Pool(processes=options.concurrency)
+            for master in masters:
+                result = p.apply_async(run_action_on_master, (action, master))
+            p.close()
+            p.join()
+            failed = False
+            for master, result in results.items():
+                if not result.successful():
+                    failed = True
+                    print master, "FAILED"
+                    break
+            if failed:
+                break

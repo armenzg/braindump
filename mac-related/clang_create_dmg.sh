38617824194249b4d6c457d3a876733f89f25e95
Rafael ?vila de Esp?ndola <respindola@mozilla.com>
2012-01-27 20:58:45 -0500

    Bug 721819 - Upgrade clang on os x.

--- mac-related/clang_create_dmg.sh
+++ mac-related/clang_create_dmg.sh
@@ -2,7 +2,7 @@
 # example script to generate clang dmg package
 # in ~/src/clang
 
-REPO_REV=145194
+REPO_REV=149148
 PREFIX=/tools/clang-3.0-$REPO_REV
 
 svn co -r $REPO_REV http://llvm.org/svn/llvm-project/llvm/trunk llvm

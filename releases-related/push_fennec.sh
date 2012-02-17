1b7e8d45cb6ff1292ab670486f81e7b245263b06
Aki Sasaki <asasaki@mozilla.com>
2012-02-17 10:24:20 -0800

    11.0b3 push_fennec.sh

--- releases-related/push_fennec.sh
+++ releases-related/push_fennec.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-export VERSION=11.0b1
-export BUILDNUM=1
+export VERSION=11.0b3
+export BUILDNUM=2
 export RD=/home/ftp/pub/mozilla.org/mobile/releases
 export CD=/home/ftp/pub/mozilla.org/mobile/candidates
+export PLATFORMS="android-xul"
 
 set -x
 set -e
@@ -10,8 +11,8 @@ set -e
 mkdir $RD/$VERSION
 cd $RD/$VERSION
 
-for platform in android android-xul; do
-    for locale in en-US multi do;
+for platform in $PLATFORMS; do
+    for locale in en-US multi ; do
         mkdir -p -m 755 $platform/$locale
         rsync -av --exclude=*gecko*  \
           $CD/$VERSION-candidates/build$BUILDNUM/$platform/$locale/ \

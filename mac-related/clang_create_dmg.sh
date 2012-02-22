2bb2a9bd8e380e58c8ee4fe3db8baf5fc2647c75
Rafael ?vila de Esp?ndola <respindola@mozilla.com>
2012-02-22 15:43:34 -0500

    Bug 727436 - Remove cpuid.h. r=rail.

--- mac-related/clang_create_dmg.sh
+++ mac-related/clang_create_dmg.sh
@@ -9,6 +9,10 @@ svn co -r $REPO_REV http://llvm.org/svn/llvm-project/llvm/trunk llvm
 svn co -r $REPO_REV http://llvm.org/svn/llvm-project/cfe/trunk clang
 svn co -r $REPO_REV http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt
 
+# Apple's gcc 4.2 doesn't include cpuid.h. Having it causes firefox's build to enable SSE.
+# While that is probably a good thing, disable it for now to reduce the differences.
+rm clang/lib/Headers/cpuid.h
+
 ln -s ../../clang llvm/tools
 ln -s ../../compiler-rt llvm/projects
 

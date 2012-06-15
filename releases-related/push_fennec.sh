14ebba8ff1d47797e39f7f0d98ca2c814976bd75
Aki Sasaki <asasaki@mozilla.com>
2012-06-15 10:57:36 -0700

    14.0b7 build 3

--- releases-related/push_fennec.sh
+++ releases-related/push_fennec.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export VERSION=14.0b7
-export BUILDNUM=2
+export BUILDNUM=3
 export RD=/home/ftp/pub/mozilla.org/mobile/releases
 export CD=/home/ftp/pub/mozilla.org/mobile/candidates
 export PLATFORMS="android"

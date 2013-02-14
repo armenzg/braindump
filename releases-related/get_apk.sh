2e2fd1e4e2eedf72a804d41476f5f52ec2a9720a
Hal Wine <hwine@mozilla.com>
2013-02-13 21:18:19 -0800

    no bug - cleaner output

--- releases-related/get_apk.sh
+++ releases-related/get_apk.sh
@@ -6,12 +6,17 @@ usage() { warn "$@" "${USAGE:-}" ; test $# -eq 0; exit $?; }
 
 test -n "${1:-}" || usage "Missing build nuber"
 
-set -eux
+trap "echo 'FAILED to download apks for $1'; exit 1" EXIT
+
+set -eu
 
 # Pull binaries down, then make sure we got an archive and not a error
 # page
-curl -O https://ftp.mozilla.org/pub/mozilla.org/mobile/candidates/$1-candidates/build1/android/multi/fennec-$1.multi.android-arm.apk
+echo "Downloading arm7 for $1"
+curl -sSO https://ftp.mozilla.org/pub/mozilla.org/mobile/candidates/$1-candidates/build1/android/multi/fennec-$1.multi.android-arm.apk
 file fennec-$1.multi.android-arm.apk | grep 'Zip archive'
-curl -O https://ftp.mozilla.org/pub/mozilla.org/mobile/candidates/$1-candidates/build1/android-armv6/multi/fennec-$1.multi.android-arm-armv6.apk
+echo "Downloading arm6 for $1"
+curl -sSO https://ftp.mozilla.org/pub/mozilla.org/mobile/candidates/$1-candidates/build1/android-armv6/multi/fennec-$1.multi.android-arm-armv6.apk
 file fennec-$1.multi.android-arm-armv6.apk | grep 'Zip archive'
+trap "" EXIT
 echo "Success! both downloaded and appear to be Zip archives"

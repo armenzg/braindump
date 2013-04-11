abd3da149b4b4d986a5e42aaa5056802f0069ba4
Hal Wine <hwine@mozilla.com>
2013-04-11 12:04:11 -0700

    push 20.0.1

--- releases-related/push_fennec.sh
+++ releases-related/push_fennec.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # VERSION & BUILDNUM are the "version" and "buildNumber" from the
 # release's cofiguration file
-export VERSION=20.0b6
+export VERSION=20.0.1
 export BUILDNUM=1
 export RD=/home/ftp/pub/mozilla.org/mobile/releases
 export CD=/home/ftp/pub/mozilla.org/mobile/candidates

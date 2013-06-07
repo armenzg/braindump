1af3beb6356a898f6bacb3b424d5c656427fd056
Ben Hearsum <bhearsum@mozilla.com>
2013-06-07 11:04:02 -0400

    update for 22.0b4

--- releases-related/push_fennec.sh
+++ releases-related/push_fennec.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # VERSION & BUILDNUM are the "version" and "buildNumber" from the
 # release's cofiguration file
-export VERSION=22.0b1
+export VERSION=22.0b4
 export BUILDNUM=1
 export RD=/home/ftp/pub/mozilla.org/mobile/releases
 export CD=/home/ftp/pub/mozilla.org/mobile/candidates
-export PLATFORMS="android android-armv6"
-#export PLATFORMS="android"
+export PLATFORMS="android android-armv6 android-x86"
+#export PLATFORMS="android android-armv6"
 
 set -x
 set -e

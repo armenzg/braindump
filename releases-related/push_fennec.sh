18cc8f6e09b05b373334270fa1f2da91ae187c84
Hal Wine <hwine@mozilla.com>
2013-08-06 12:30:18 -0700

    fennec 23.0 release

--- releases-related/push_fennec.sh
+++ releases-related/push_fennec.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # VERSION & BUILDNUM are the "version" and "buildNumber" from the
 # release's cofiguration file
-export VERSION=23.0b10
+export VERSION=23
 export BUILDNUM=1
 export RD=/home/ftp/pub/mozilla.org/mobile/releases
 export CD=/home/ftp/pub/mozilla.org/mobile/candidates
-export PLATFORMS="android android-armv6 android-x86"
-#export PLATFORMS="android android-armv6"
+#export PLATFORMS="android android-armv6 android-x86"
+export PLATFORMS="android android-armv6"
 
 set -x
 set -e

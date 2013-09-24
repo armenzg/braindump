08b56c4d3f23dae52cdc3610e7f0823b9673b79e
Chris Cooper <ccooper@deadsquid.com>
2013-09-24 18:36:24 -0400

    Make buildduty report cron silent under ideal conditions

--- reports/buildduty_report.sh
+++ reports/buildduty_report.sh
@@ -3,7 +3,7 @@ set -e
 
 cd $HOME/buildduty
 if [ ! -d braindump ]; then
-  hg clone https://hg.mozilla.org/build/braindump/
+  hg clone http://hg.mozilla.org/build/braindump/
   ln -s braindump/reports/buildduty_report.py
 else
   hg -q -R braindump pull -u
@@ -12,14 +12,14 @@ fi
 if [ ! -d bztools ]; then
   git clone https://github.com/ccooper/bztools.git
 else
-  pushd bztools  
-  git reset --hard
-  git clean -f -d
-  git pull
-  popd
+  cd bztools
+  git reset --hard --quiet
+  git clean -f -d --quiet
+  git pull --quiet
+  cd ..
 fi
 
-source $HOME/buildduty/bin/activate
+source bin/activate
 export PYTHONPATH=$HOME/buildduty/bztools
 
 python buildduty_report.py

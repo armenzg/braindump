90fa5fe241c5b237ba7f9c42389b4afbd7a6293a
Aki Sasaki <asasaki@mozilla.com>
2013-12-04 12:01:03 -0800

    26.0b10

--- releases-related/push_fennec.sh
+++ releases-related/push_fennec.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # VERSION & BUILDNUM are the "version" and "buildNumber" from the
 # release's cofiguration file
-export VERSION=25.0.1
+export VERSION=26.0b10
 export BUILDNUM=1
 export RD=/home/ftp/pub/mozilla.org/mobile/releases
 export CD=/home/ftp/pub/mozilla.org/mobile/candidates

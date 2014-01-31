d2d13546a690f35b399724676d0091a6ffc7daf1
Ben Hearsum <bhearsum@mozilla.com>
2014-01-31 16:42:36 -0500

    Add initial wait times; fix build times to work with multiple builders.

--- buildbot-related/get_build_times_for_builders.py
+++ buildbot-related/get_build_times_for_builders.py
@@ -18,6 +18,9 @@ class Sourcestamps(object):
 class SourcestampChanges(object):
     pass
 
+class Changes(object):
+    pass
+
 if __name__ == '__main__':
     import sys
 
@@ -32,22 +35,38 @@ if __name__ == '__main__':
     buildsets = Table('buildsets', metadata, autoload=True)
     sourcestamps = Table('sourcestamps', metadata, autoload=True)
     sourcestampchanges = Table('sourcestamp_changes', metadata, autoload=True)
+    changes = Table('changes', metadata, autoload=True)
     mapper(Builds, builds)
     mapper(Buildrequests, buildrequests)
     mapper(Buildsets, buildsets)
     mapper(Sourcestamps, sourcestamps)
     mapper(SourcestampChanges, sourcestampchanges, primary_key=[sourcestampchanges.c.sourcestampid, sourcestampchanges.c.changeid])
+    mapper(Changes, changes)
     session = sessionmaker(bind=engine)()
 
+    print "Build times:"
+    print "builder name, build number, built revision, build time, # of builds coallesced"
     for b, br, bs, ss in session.query(Builds, Buildrequests, Buildsets, Sourcestamps)\
                          .outerjoin(Buildrequests, Builds.brid==Buildrequests.id)\
                          .join(Buildsets, Buildrequests.buildsetid==Buildsets.id)\
                          .join(Sourcestamps, Buildsets.sourcestampid==Sourcestamps.id)\
                          .filter(Buildrequests.buildername.in_(builders))\
-                         .filter(Builds.start_time > since)\
-                         .group_by(Builds.number):
+                         .filter(Builds.start_time > since):
 
         elapsed = b.finish_time - b.start_time    
         final_rev = ss.revision
         total_revs = session.query(Builds.id).join(Buildrequests, Builds.brid==Buildrequests.id).filter(Builds.number==b.number).filter(Buildrequests.buildername==br.buildername).count()
         print "%s,%s,%s,%s,%s" % (br.buildername, b.number, final_rev, elapsed, total_revs)
+
+    print "Wait times:"
+    print "builder name, revision, wait time"
+    for b, br, ss, c in session.query(Builds, Buildrequests, Sourcestamps, Changes)\
+                         .outerjoin(Buildrequests, Builds.brid==Buildrequests.id)\
+                         .join(Buildsets, Buildrequests.buildsetid==Buildsets.id)\
+                         .join(Sourcestamps, Buildsets.sourcestampid==Sourcestamps.id)\
+                         .join(SourcestampChanges, Sourcestamps.id==SourcestampChanges.sourcestampid)\
+                         .join(Changes, SourcestampChanges.changeid==Changes.changeid)\
+                         .filter(Buildrequests.buildername.in_(builders))\
+                         .filter(Builds.start_time > since):
+        if b.finish_time:
+            print "%s,%s,%s" % (br.buildername, ss.revision, b.start_time - c.when_timestamp)

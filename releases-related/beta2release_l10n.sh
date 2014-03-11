0021700be876dffb5b2d4435e1d96d9ef27eeca0
Hal Wine <hwine@mozilla.com>
2014-03-11 12:57:20 -0700

    bug 982272 - locale merge aborts script; r=rail

--- releases-related/beta2release_l10n.sh
+++ releases-related/beta2release_l10n.sh
@@ -14,6 +14,11 @@ mkdir -p $wd
 cd $wd
 
 for l in `wget -q -O- http://$HG_HOST/releases/$repo/raw-file/default/browser/locales/shipped-locales | grep -v en-US | awk '{print $1}'`; do
+    if test -d $l -a -d $l.beta; then
+        # allow re-running if script bombs out. Assumption is if both
+        # repos exist, locale has been dealt with.
+        continue
+    fi
     hg clone http://$HG_HOST/$release_repo_path/$l
     hg clone http://$HG_HOST/$beta_repo_path/$l $l.beta
     release_rev=`hg -R $l id -i -r default`
@@ -25,9 +30,16 @@ for l in `wget -q -O- http://$HG_HOST/releases/$repo/raw-file/default/browser/lo
         hg -R $l up -C -r $beta_rev
         HGMERGE=true hg -R $l merge -r $release_rev
         hg -R $l revert -a -y --no-backup -r $beta_rev
-        hg -R $l commit -m "Merge from mozilla-beta" -u "$HG_USER"
-        hg -R $l commit -u $HG_USER -m "Merge from mozilla-beta. CLOSED TREE a=release"
+        # okay if commit "fails" (it will if there is nothing to commit)
+        ec=0
+        hg -R $l commit -u "$HG_USER" -m "Merge from mozilla-beta. CLOSED TREE a=release" || ec=$? | tee -a merged_l10n_locales
+        echo "Merge on locale '$l'; exit code '$ec'" >> merged_l10n_locales
     fi
     hg -R $l diff -r $beta_rev -r default
     hg -R $l push -f -e "ssh -l ffxbld -i ~/.ssh/ffxbld_dsa" ssh://$HG_HOST/$release_repo_path/$l
 done
+
+if test -r merged_l10n_locales; then
+    echo "The following locales needed merging:"
+    cat merged_l10n_locales
+fi

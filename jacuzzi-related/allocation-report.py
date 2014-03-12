0bdfa593995aac0cbc1f14d0f4db9a906675bea0
Ben Hearsum <bhearsum@mozilla.com>
2014-03-12 10:55:46 -0400

    Improve jacuzzi verificatino

--- jacuzzi-related/allocation-report.py
+++ jacuzzi-related/allocation-report.py
@@ -29,21 +29,28 @@ for b in os.listdir(path.join(d, "builders")):
         if b in machines[m]:
             builder_machines.add(m)
     if builders[b] != builder_machines:
-        logging.warning("Mismatch in builder machines for %s: %s", b, builders[b].difference(builder_machines))
+        logging.warning("Mismatch in builder machines for %s: %s", b, sorted(builders[b].difference(builder_machines)))
 
-# Verify that machine allocations match the builders'
+# Verify that machine allocations match the builders', and also obey our other constraints
 for m in machines:
     machine_builders = set()
     for b in builders:
         if m in builders[b]:
             machine_builders.add(b)
     if machines[m] != machine_builders:
-        logging.warning("Mismatch in machine builders for %s: %s", m, machines[m].difference(machine_builders))
+        logging.warning("Mismatch in machine builders for %s: %s", m, sorted(machines[m].difference(machine_builders)))
+    for b in machine_builders:
+        if "l10n" not in b:
+            break
+    else:
+        logging.warning("%s only has l10n builders configured", m)
+    if len(machine_builders) > 2:
+        logging.warning("%s has more than 2 builders", m)
 
 # And verify that all of the machines in machines/ are listed in the list of allocated machines
 all_machines = set(machines.keys())
 if allocated != all_machines:
-    logging.warning("Mismatch in allocated/all vs. machines/*: %s", allocated.difference(all_machines))
+    logging.warning("Mismatch in allocated/all vs. machines/*: %s, %s", sorted(allocated.difference(all_machines)), sorted(all_machines.difference(allocated)))
 
 for m in sorted(machines):
     print "%s is allocated to %s builders" % (m, len(machines[m]))

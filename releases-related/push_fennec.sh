ae15a4f2a27475c08ac80f79fb63f626f2749d52
Aki Sasaki <asasaki@mozilla.com>
2014-05-21 15:37:13 -0700

    30.0b6

--- releases-related/push_fennec.sh
+++ releases-related/push_fennec.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # VERSION & BUILDNUM are the "version" and "buildNumber" from the
 # release's cofiguration file
-export VERSION=29.0.1
-export BUILDNUM=1
+export VERSION=30.0b6
+export BUILDNUM=2
 export RD=/home/ftp/pub/mozilla.org/mobile/releases
 export CD=/home/ftp/pub/mozilla.org/mobile/candidates
 export PLATFORMS="android android-armv6 android-x86"

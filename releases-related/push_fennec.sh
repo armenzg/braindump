f5943ab3bf71066aa6d8e30b6002c2d40edfd967
Hal Wine <hwine@mozilla.com>
2014-06-04 14:18:21 -0700

    30.0b10 push

--- releases-related/push_fennec.sh
+++ releases-related/push_fennec.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 # VERSION & BUILDNUM are the "version" and "buildNumber" from the
 # release's cofiguration file
-export VERSION=30.0b6
-export BUILDNUM=2
+export VERSION=30.0b10
+export BUILDNUM=1
 export RD=/home/ftp/pub/mozilla.org/mobile/releases
 export CD=/home/ftp/pub/mozilla.org/mobile/candidates
 export PLATFORMS="android android-armv6 android-x86"
-#export PLATFORMS="android android-armv6"
 
 set -x
 set -e

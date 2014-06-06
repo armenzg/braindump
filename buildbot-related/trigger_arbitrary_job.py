65d070fcefdf52a976bff350f93b751c2646857a
Armen Zambrano Gasparnian <armenzg@mozilla.com>
2014-06-06 13:15:57 -0400

    Prevent triggering arbitrary builds for files that cannot be reached

--- buildbot-related/trigger_arbitrary_job.py
+++ buildbot-related/trigger_arbitrary_job.py
@@ -28,22 +28,36 @@ revision = args.revision
 buildername = args.buildername
 files = args.files or []
 
+def all_files_are_reachable(files):
+    ''' All files should be reachable or requirying basic http authentication
+    '''
+    reachable_files = True
+    for file in files:
+        r = requests.head(file)
+        if r.status_code != 200 and r.status_code != 401:
+            # 401 files to us can be reachable within the releng network
+            print "Status code: %d - The following files cannot be reached: %s" \
+                    % (r.status_code, file)
+            reachable_files = False
+
+    return reachable_files
+
+assert all_files_are_reachable(files), "All files should be reachable"
+
 # Check that files is either 0 (build job) or 2 (test job: installer + tests.zip)
 # XXX: Talos might be one file, I'm not sure
 assert len(files) != 1, "You can either have no files or two files specified"
 assert len(files) <= 2, "You have specified more than 2 files"
 
 payload = {}
-# Adding the properties here are to allow tbpl to show the job
-# as they're running
-# Currently broken - bug 981825
+# Adding the properties here is so tbpl can show the job as they run
 payload['properties'] = json.dumps({"branch": branch, "revision": revision})
 payload['files'] = json.dumps(files)
 
 url = r'''https://secure.pub.build.mozilla.org/buildapi/self-serve/%s/builders/%s/%s''' % \
         (branch, buildername, revision)
 r = requests.post(url, data=payload)
-print r.status_code
-# We can't yet see the jobs - bug 981825
-#print "See your running jobs in here:"
-#print "https://secure.pub.build.mozilla.org/buildapi/revision/%s/%s" % (branch, revision)
+
+print "You return code is: %s" % r.status_code
+print "See your running jobs in here:"
+print "https://secure.pub.build.mozilla.org/buildapi/revision/%s/%s" % (branch, revision)

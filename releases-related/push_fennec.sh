20300ac5be4e39412dcf33569a55b6df4c0418c6
Hal Wine <hwine@mozilla.com>
2014-06-16 14:04:11 -0700

    bug 996137 - ff 30.0 release

--- releases-related/push_fennec.sh
+++ releases-related/push_fennec.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # VERSION & BUILDNUM are the "version" and "buildNumber" from the
 # release's cofiguration file
-export VERSION=31.0b1
-export BUILDNUM=1
+export VERSION=30.0
+export BUILDNUM=2
 export RD=/home/ftp/pub/mozilla.org/mobile/releases
 export CD=/home/ftp/pub/mozilla.org/mobile/candidates
 export PLATFORMS="android android-armv6 android-x86"

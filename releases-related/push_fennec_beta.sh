1ead1173434662b2dea80679c0abb4e6140a7b22
Nick Thomas <nthomas@mozilla.com>
2014-09-10 08:53:50 +1200

    Push Fennec 33.0b2 build1

--- releases-related/push_fennec_beta.sh
+++ releases-related/push_fennec_beta.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # VERSION & BUILDNUM are the "version" and "buildNumber" from the
 # release's cofiguration file
-export VERSION=33.0b1
-export BUILDNUM=2
+export VERSION=33.0b2
+export BUILDNUM=1
 export RD=/home/ftp/pub/mozilla.org/mobile/releases
 export CD=/home/ftp/pub/mozilla.org/mobile/candidates
 export PLATFORMS="android android-x86"

89a2dc8505b8377375a8f823baa9b643060e7f69
Rail Aliiev <rail@mozilla.com>
2014-10-06 13:47:42 -0400

    Don't require prefix

--- releases-related/gaia_l10n_list.py
+++ releases-related/gaia_l10n_list.py
@@ -3,19 +3,23 @@
 import argparse
 import requests
 
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--all-locales-url",
                         default="https://raw.githubusercontent.com/mozilla-b2g/gaia/master/locales/languages_all.json",
                         help="URL of Gaia's languages_all.json")
-    parser.add_argument("-p", "--prefix", required=True,
+    parser.add_argument("-p", "--prefix", default="",
                         help="Prefix to be added to each locale to be used as"
                         " a part of hg.m.o, e.g. releases/gaia-l10n/v2_0")
     args = parser.parse_args()
+    prefix = args.prefix
+    if prefix:
+        prefix = prefix.rstrip("/") + "/"
 
     locales = requests.get(args.all_locales_url).json()
     for l in sorted(e for e in locales.keys() if e != "en-US"):
-        print "{}/{}".format(args.prefix, l)
+        print "{}{}".format(args.prefix, l)
 
 if __name__ == '__main__':
     main()

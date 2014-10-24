3aee209d479cd8f25bfd1b66b1972caf430c2e19
Rail Aliiev <rail@mozilla.com>
2014-10-24 11:44:34 -0400

    Add a tool to generate hg/git tarballs

---
+++ utils/generate-hg-git-shared.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+set -x
+
+WRK_DIR=~/tmp/bundle
+TOOLS_DIR=~/work/mozilla/build/tools
+
+GITTOOL=$TOOLS_DIR/buildfarm/utils/gittool.py
+HGTOOL=$TOOLS_DIR/buildfarm/utils/hgtool.py
+REPO_DIR=$WRK_DIR/git-shared/repo
+export HG_SHARE_BASE_DIR=$WRK_DIR/hg-shared
+
+DEVICES="dolphin emulator emulator-jb emulator-kk flame flame-kk hamachi helix nexus-4 wasabi"
+
+# Clone mozilla-central
+python $HGTOOL https://hg.mozilla.org/mozilla-central mozilla-central
+
+# Clone B2G
+python $GITTOOL https://git.mozilla.org/b2g/B2G.git B2G
+
+mkdir -p $REPO_DIR
+
+cd B2G
+rm -rf .repo
+ln -sf $REPO_DIR .repo
+# No need to update the working copy
+export REPO_SYNC_FLAGS=--network-only
+for device in $DEVICES; do
+    time ./config.sh -q $device ../mozilla-central/b2g/config/$device/sources.xml < /dev/null
+done
+
+cd $REPO_DIR/..
+rm -rfv repo/manifest*
+find repo -name clone.bundle -o -name '*.lock' -print -delete
+time tar cf git-shared-repo.tar repo/
+
+cd $HG_SHARE_BASE_DIR/mozilla-central/.hg
+time tar cf ../mozilla-central.tar .
+
+# upload $HG_SHARE_BASE_DIR/mozilla-central.tar and $WRK_DIR/git-shared/repo.tar
+time s3cmd put $HG_SHARE_BASE_DIR/mozilla-central.tar s3://mozilla-releng-tarballs/mozilla-central.tar
+time s3cmd put $WRK_DIR/git-shared/git-shared-repo.tar s3://mozilla-releng-tarballs/git-shared-repo.tar

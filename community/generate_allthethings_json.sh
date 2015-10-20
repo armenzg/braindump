025f669d2f5c12a5de7b46732bb7b062cb0e7b77
Armen Zambrano Gasparnian <armenzg@mozilla.com>
2015-10-20 13:34:50 -0400

    generate_allthethings_json.sh to only run on change for cruncher

--- community/generate_allthethings_json.sh
+++ community/generate_allthethings_json.sh
@@ -16,25 +16,50 @@ then
    python_path="-p $python_path"
 fi
 
+workdir="$HOME/.mozilla/releng"
+allthethings="$workdir/repos/buildbot-configs/allthethings.json"
+# If you want to use a modified braindump repo change this path
+dump_script="$workdir/repos/braindump/buildbot-related/dump_allthethings.sh"
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $script_dir
-workdir="$HOME/.mozilla/releng"
-# Quiet set up
-./setup_buildbot_environment.sh $quiet "$python_path" -w $workdir
 
-cd $workdir/repos/buildbot-configs
-source $workdir/venv/bin/activate
-# If you want to use a modified braindump repo change this
-$workdir/repos/braindump/buildbot-related/dump_allthethings.sh
-allthethings="$workdir/repos/buildbot-configs/allthethings.json"
+function make_allthethings() {
+    cd $workdir/repos/buildbot-configs
+    source $workdir/venv/bin/activate
+    echo "making all the things!"
+    # Generate allthethings.json
+    $dump_script
+}
 
-# If we're executing this in cruncher
-if [ -d /var/www/html/builds/ ]
-then
-    # Do not overwrite the older allthethings
-    cp $allthethings /var/www/html/builds/allthethings.new.json
-    gzip -c $allthethings > /var/www/html/builds/allthethings.new.json.gz
-    chmod 644 /var/www/html/builds/allthethings.new.json*
+# If we're executing this in cruncher we don't need to call
+# setup_buildbot_environment.sh (since we've already done so)
+# The main difference here is the "updated" or not logic
+if [ -d /var/www/html/builds/ ]; then
+    cd $workdir/repos
+    # Logic borrowed from catlee
+    updated=0
+    for d in buildbot-configs buildbotcustom tools; do
+        t=$(mktemp)
+        hg -R $d pull -q
+        prev_rev=$(hg -R $d id)
+        hg -R $d update -q
+        cur_rev=$(hg -R $d id)
+        if [ "$prev_rev" != "$cur_rev" ]; then
+            echo "$d updated something"
+            updated=1
+        fi
+    done
+
+    if [ "$updated" = "1" ]; then
+        make_allthethings
+        # Do not overwrite the older allthethings
+        cp $allthethings /var/www/html/builds/allthethings.new.json
+        gzip -c $allthethings > /var/www/html/builds/allthethings.new.json.gz
+        chmod 644 /var/www/html/builds/allthethings.new.json*
+    fi
 else
+    ./setup_buildbot_environment.sh $quiet "$python_path" -w $workdir
+
+    make_allthethings
     echo "The file is now in here $allthethings"
 fi

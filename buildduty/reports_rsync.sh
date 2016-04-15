136e7bea4e8de364b65988587f5cc8321f0e9e49
Chris Cooper <ccooper@deadsquid.com>
2016-04-15 11:30:53 -0400

    Remove logger command

--- buildduty/reports_rsync.sh
+++ buildduty/reports_rsync.sh
@@ -8,4 +8,4 @@ trap "/usr/bin/lockfile-remove $LOCK" EXIT
 REMOTE_USER=syncbld
 REMOTE_SERVER=cruncher-aws.srv.releng.usw2.mozilla.com
 
-/usr/bin/rsync --progress -r --links --delete ${REMOTE_USER}@${REMOTE_SERVER}:/var/www/html/builds/ /mnt/netapp/relengweb/builddata/reports/ 2>&1 | logger -t pull_builddata_reports
+/usr/bin/rsync -r --links --delete ${REMOTE_USER}@${REMOTE_SERVER}:/var/www/html/builds/ /mnt/netapp/relengweb/builddata/reports/
